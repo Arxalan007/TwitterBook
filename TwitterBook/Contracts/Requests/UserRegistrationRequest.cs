072a2ae17bfeae894ba5102ddcdba4fb55dce971
marsalan <arxalan.arain987@gmail.com>
2023-09-07 01:03:13 +0500

    [feat]: Jwt Authentication Login with user added.

--- TwitterBook/Contracts/Requests/UserRegistrationRequest.cs
+++ TwitterBook/Contracts/Requests/UserRegistrationRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace TwitterBook.Contracts.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterBook.Contracts.Requests
 {
     public class UserRegistrationRequest
     {
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
--- TwitterBook/Services/IdentityService.cs
+++ TwitterBook/Services/IdentityService.cs
@@ -23,6 +23,33 @@ namespace TwitterBook.Services
             _jwtSettings = jwtSettings;
         }
 
+        private AuthenticationResult GenerateAuthenticationResultForUser(IdentityUser newUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, newUser.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, newUser.Email),
+                    new Claim("id", newUser.Id)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials =
+                    new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return new AuthenticationResult
+            {
+                Success = true,
+                Token = tokenHandler.WriteToken(token)
+            };
+        }
+
         public async Task<AuthenticationResult> RegisterAsync(string email, string password)
         {
             var existingUser = await _userManager.FindByEmailAsync(email);
@@ -47,28 +74,28 @@ namespace TwitterBook.Services
                     Errors = createdUser.Errors.Select(x => x.Description)
                 };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new []
+            return GenerateAuthenticationResultForUser(newUser);
+        }
+
+        public async Task<AuthenticationResult> LoginAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+                return new AuthenticationResult
                 {
-                    new Claim(JwtRegisteredClaimNames.Sub, newUser.Email),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                    new Claim(JwtRegisteredClaimNames.Email, newUser.Email),
-                    new Claim("id", newUser.Id)
-                }),
-                Expires = DateTime.UtcNow.AddHours(2),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
+                    Errors = new[] { "User does not exist." }
+                };
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return new AuthenticationResult
-            {
-                Success = true,
-                Token = tokenHandler.WriteToken(token)
-            };
+            var userHasValidPassword = await _userManager.CheckPasswordAsync(user, password);
+            
+            if(!userHasValidPassword)
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User/Password combination is wrong." }
+                };
+            
+            return GenerateAuthenticationResultForUser(user);
         }
     }
 }
