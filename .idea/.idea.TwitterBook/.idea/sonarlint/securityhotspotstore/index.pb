87c71af7059a830fa7ed153760f49395755453d9
Arxalan007 <arxalan.arain987@gmail.com>
2024-10-15 16:59:51 +0500

    update.

---
+++ .idea/.idea.TwitterBook/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+F
+TwitterBook/Program.cs,e\4\e4393abac54b8a9ea7a139401b0833e9492c389a
+F
+TwitterBook/Startup.cs,9\e\9ea9a7faae30e7f75cddfcb853c70ecd170feeab
+V
+&TwitterBook/Installers/MvcInstaller.cs,c\3\c30f67026ddc7ec5541a27cca6fd1ff701b0f526
+R
+"TwitterBook/Options/JwtSettings.cs,1\e\1ee0ec5296d09ca7c5913bd2ccadec5bfe0b5549
+W
+'TwitterBook/Services/IdentityService.cs,9\8\98e78bd44bc4df881941242b1ea1f54d253b89b8
+O
+TwitterBook/Data/DataContext.cs,8\3\8379e2b9939040c2560463b11244fa171ba7e266
--- TwitterBook/Startup.cs
+++ TwitterBook/Startup.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using TwitterBook.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Sustainsys.Saml2;
+using Sustainsys.Saml2.AspNetCore2;
+using Sustainsys.Saml2.Metadata;
+using Sustainsys.Saml2.Saml2P;
 using TwitterBook.Installers;
 using TwitterBook.Options;
 
@@ -26,6 +26,32 @@ namespace TwitterBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(opt =>
+                {
+                    // Default scheme that maintains session is cookies.
+                    opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                    // If there's a challenge to sign in, use the Saml2 scheme.
+                    opt.DefaultChallengeScheme = Saml2Defaults.Scheme;
+                }).AddCookie().AddSaml2(opt =>
+                {
+                    // Set up our EntityId, this is our application.
+                    opt.SPOptions.EntityId = new EntityId("https://localhost:5001/Saml2");
+
+                    // Single logout messages should be signed according to the SAML2 standard, so we need
+                    // to add a certificate for our app to sign logout messages with to enable logout functionality.
+                    opt.SPOptions.ServiceCertificates.Add(new X509Certificate2("Sustainsys.Saml2.Tests.pfx"));
+
+                    // Add an identity provider.
+                    opt.IdentityProviders.Add(new IdentityProvider(
+                        // The identityprovider's entity id.
+                        new EntityId("https://stubidp.sustainsys.com/Metadata"),
+                        opt.SPOptions)
+                    {
+                        // Load config parameters from metadata, using the Entity Id as the metadata address.
+                        LoadMetadata = true
+                    });
+                });
             services.InstallServicesInAssembly(Configuration);
         }
 
@@ -41,11 +67,14 @@ namespace TwitterBook
             {
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+            
+            var saml = new Saml2AuthenticationRequest();
+            Configuration.GetSection(nameof(Saml2AuthenticationRequest)).Bind(saml);
             
             app.UseAuthentication();
             app.UseAuthorization();
